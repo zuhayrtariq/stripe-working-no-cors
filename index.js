801899ab9b2cef2eb8b57ff00a7b894883f5a3cc
zuhayrtariq <111240413+zuhayrtariq@users.noreply.github.com>
2022-12-07 04:56:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const proxyUrl = 'https://zuhayrcors.herokuapp.com/';
 const express = require("express");
 const app = express();
 const path = require("path");
-const stripe = require("stripe")("sk_test_51LXJMNHaiJQbqv8nU6Hnl7ZnL2CI9ulx2E9t4uGkLIHU0euDvV9a9vW1ZBBvi602m9vWKNIQAfvBjy6lrVD0vFzf002fDuknqP");
+//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
+const STRIPE_PRIVATE_KEY = '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';
+const stripe = require("stripe")(STRIPE_PRIVATE_KEY);
 // const cors = require('cors');
 // app.use(
 //     cors({
@@ -11,10 +12,10 @@ const stripe = require("stripe")("sk_test_51LXJMNHaiJQbqv8nU6Hnl7ZnL2CI9ulx2E9t4
 //         methods: "GET,HEAD,OPTIONS,PUT,PATCH,DELETE"
 //     })
 // )
-const YOUR_DOMAIN = "https://serversidestripe.herokuapp.com";
+const YOUR_DOMAIN = "https://tfm.express";
 
 // static files
-app.use(express.static(path.join(__dirname, "client")));
+//app.use(express.static(path.join(__dirname, "client")));
 
 // middleware
 app.use(express.json());
@@ -27,27 +28,74 @@ app.use(function(req, res, next) {
 
 // routes
 // app.options('*', cors());
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Stripe Integration Server"));
+
+// const storeItems = new Map([
+//     [0, { priceInCents: 50000, name: "Service Charges" }],
+
+//     [1, { priceInCents: 50000, name: "Lead Generation" }],
+//     [2, { priceInCents: 40000, name: "Event Promotion" }],
+//     [3, { priceInCents: 50000, name: "Social Competition" }],
+//     [4, { priceInCents: 40000, name: "Site Traffic" }],
+//     [5, { priceInCents: 40000, name: "Social Growth" }],
 
 
+//     [6, { priceInCents: 15000, name: "Lead Page" }],
+//     [7, { priceInCents: 30000, name: "Instagram Story Animation" }],
+//     [8, { priceInCents: 30000, name: "Carousel Creative" }],
+//     [9, { priceInCents: 50000, name: "Lead Automation" }],
+// ])
 app.post("/payment", async(req, res) => {
     const { product } = req.body;
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [{
-            price_data: {
-                currency: "usd",
-                product_data: {
-                    name: "Total Cost",
+    product.amount = product.amount * 100;
+    const storeItems = new Map([
+        [0, { priceInCents: 50000, name: "Service Charges" }],
 
+        [1, { priceInCents: 50000, name: "Lead Generation" }],
+        [2, { priceInCents: 40000, name: "Event Promotion" }],
+        [3, { priceInCents: 50000, name: "Social Competition" }],
+        [4, { priceInCents: 40000, name: "Site Traffic" }],
+        [5, { priceInCents: 40000, name: "Social Growth" }],
+
+
+        [6, { priceInCents: 15000, name: "Lead Page" }],
+        [7, { priceInCents: 30000, name: "Instagram Story Animation" }],
+        [8, { priceInCents: 30000, name: "Carousel Creative" }],
+        [9, { priceInCents: 50000, name: "Lead Automation" }],
+        [10, { priceInCents: product.amount, name: "Media Budget" }],
+    ])
+
+    const session = await stripe.checkout.sessions.create({
+        customer_email: req.body.clientEmail,
+        payment_method_types: ["card"],
+        line_items: req.body.items.map(item => {
+            const storeItem = storeItems.get(item.id)
+            return {
+                price_data: {
+                    currency: "aud",
+                    product_data: {
+                        name: storeItem.name,
+                    },
+                    unit_amount: storeItem.priceInCents,
                 },
-                unit_amount: (product.amount + 500) * 100,
-                // unit_amount: product.amount * 100,
-            },
-            quantity: product.quantity,
-        }, ],
+                quantity: 1,
+            }
+        }),
+        // line_items: [{
+        //     price_data: {
+        //         currency: "usd",
+        //         product_data: {
+        //             name: "Total Cost",
+
+        //         },
+        //       unit_amount: (product.amount + 500) * 100,
+        // unit_amount: product.amount * 100,
+        //             },
+        //             quantity: product.quantity,
+        //         },
+        // ],
         mode: "payment",
-        success_url: `${YOUR_DOMAIN}/success.html`,
+        success_url: `${YOUR_DOMAIN}/confirmpaymentmail.php`,
         cancel_url: `${YOUR_DOMAIN}/cancel.html`,
     });
 
